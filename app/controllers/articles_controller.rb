9dc426e04daf20ef5e61fb8e56f1908bdef79328
Bernardo Viseu <bernardo2viseu@gmail.com>
2019-02-09 18:48:41 +0000

    já da para ver o index de articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -20,7 +20,7 @@ class ArticlesController < ApplicationController
           render 'new'
         end
       end
-       
+
       private
         def article_params
           params.require(:article).permit(:title, :text)
